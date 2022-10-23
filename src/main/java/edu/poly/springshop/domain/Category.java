0f75f28575bfda407b7788949515ccb91f5fcc6d
Tien Tran <maytien2002@gmail.com>
2022-10-23 15:46:33 +0700

    init project and add category fu

---
+++ src/main/java/edu/poly/springshop/domain/Category.java
@@ -0,0 +1,31 @@
+package edu.poly.springshop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Enumerated
+    @Column(name = "status", nullable = false)
+    private CategoryStatus status;
+
+
+
+}
